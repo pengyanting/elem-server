ad8943e86aaa2e1f86016a8fa1eb4cc1159331b3
pengyanting <18035072396@163.com>
2017-08-01 18:06:27 +0800

    login

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,10 +22,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// app.all('*', function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//   res.header("X-Powered-By", ' 3.2.1')
+//   res.header("Content-Type", "application/json;charset=utf-8");
+//   next();
+// });
 app.use('/', routes);
 app.use('/users', users);
-
+app.use(session({  
+    secret: 'recommend 128 bytes random string',  
+    cookie: {maxAge: 3600 * 1000}  
+})) 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,70 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../modules/db');
+var User = require('../modules/users');   // 调用刚才封装好的user类  
+router.get('/login', function (req, res, next) {
+  res.send('aa')
+})
+//登录
 router.post('/login', function (req, res, next) {
-  //解决跨域
-  res.setHeader('Access-Control-Allow-Origin', 'http://10.7.34.231:8089');
-  console.log(req.body.phone)
-  db.con (function (connection) {
-    connection.query('select id from user where phone= "'+req.body.phone+'"',function(error,result){
-      if(error) throw error
-      if(JSON.stringify(result)=='[]'){//手机号未注册，自动注册
-        connection.query('INSERT INTO user(name,phone) VALUES (?,?)',['aa',req.body.phone],function(error,result){
-          if(error) throw error
-          result=JSON.parse(JSON.stringify(result).replace('OkPacket'));
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  var newUser = new User({   //生成一个User的实例，并赋给他name和passowrd属性  
+    name: Math.random().toString(16).substring(2),
+    phone: req.body.phone  //这里的password是加密过的（存储在数据库里也是加密过后的形式）  
+  })
+  newUser.get(function (err, result) {
+    if (result == null) {//手机号未注册
+      newUser.save(function (errs, result) {
+        if (result.affectedRows == 1) {
           res.send({
-            message:'success',
-            id:result.insertId
+            message: 'success',
+            code: 0,
+            result: {
+              id: result.insertId,
+              phone: req.body.phone,
+              password: '',
+              name: newUser.name
+            }
           })
-        })
-      }else{//手机号因存在
-        res.send({
-          message:'success',
-          id:result[0].id
-        })
+        }
+      })
+    } else {//手机号已注册
+      //如果前端传入密码，通过密码登录
+      var resData = {}
+      if (req.body.password) {
+        if (req.body.password == result.password) {
+          resData.message = 'success'
+          resData.code = 0
+          resData.result = result
+        } else {
+          resData.message = '输入的密码有误'
+          resData.code = 1
+        }
+      } else {
+        //否则通过验证码登录，校验验证码
+        if (req.body.checkCode == '111111') {
+          resData.message = 'success'
+          resData.code = 0
+          resData.result = result
+        } else {
+          resData.message = '输入的验证码有误'
+          resData.code = 1
+        }
       }
-    })
+      res.send(resData)
+    }
   })
 })
-
+router.post('/getUser', function (req, res, next) {
+  var newUser = new User({   //生成一个User的实例，并赋给他name和passowrd属性  
+    phone: req.body.phone  //这里的password是加密过的（存储在数据库里也是加密过后的形式）  
+  })
+  newUser.get(function(err,result){
+     if(err){
+       console.log(err);
+       return;
+     }
+      console.log(result)
+  })
+})
 module.exports = router;
